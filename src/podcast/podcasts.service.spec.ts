1604d67b0defab1542f5d67fb131f798b4d12f58
Kirim-Lee <uthi1004@gmail.com>
2021-01-15 00:01:58 +0900

    get podcast , create podcast

--- src/podcast/podcasts.service.spec.ts
+++ src/podcast/podcasts.service.spec.ts
@@ -9,9 +9,16 @@ import { PodcastsService } from './podcasts.service';
 const mockRepository = {
   findOne: jest.fn(),
   find: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
 };
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const allPodcast = [
+  { id: 1, title: 'abc', category: 'sf' },
+  { id: 2, title: 'bcd', category: 'sf' },
+];
 
 const serverErrorText = 'Internal server error occurred.';
 
@@ -44,10 +51,6 @@ describe('PodcastService', () => {
 
   describe('getAllPodcasts', () => {
     it('should return all podcasts', async () => {
-      const allPodcast = [
-        { id: 1, title: 'abc', category: 'sf' },
-        { id: 2, title: 'bcd', category: 'sf' },
-      ];
       podcastRepository.find.mockResolvedValue(allPodcast);
 
       const result = await podcastService.getAllPodcasts();
@@ -58,7 +61,7 @@ describe('PodcastService', () => {
       expect(result.podcasts).toEqual(allPodcast);
     });
 
-    it('should return error', async () => {
+    it('should return error when accure exception', async () => {
       podcastRepository.find.mockImplementation(() => {
         throw Error();
       });
@@ -66,6 +69,81 @@ describe('PodcastService', () => {
 
       expect(result.ok).toBeFalsy();
       expect(result.error).toBe(serverErrorText);
+    });
+  });
+
+  describe('createPodcast', () => {
+    const newPodcast = {
+      title: 'new title',
+      category: 'new category',
+    };
+
+    it('should create podcast', async () => {
+      podcastRepository.create.mockReturnValue(newPodcast);
+      podcastRepository.save.mockImplementation(podcast => ({
+        id: 3,
+        ...podcast,
+      }));
+
+      const result = await podcastService.createPodcast(newPodcast);
+
+      expect(podcastRepository.create).toHaveBeenCalledTimes(1);
+      expect(podcastRepository.create).toHaveBeenCalledWith(newPodcast);
+      expect(podcastRepository.save).toBeCalledTimes(1);
+      expect(podcastRepository.save).toHaveBeenCalledWith(newPodcast);
+      expect(result.ok).toBeTruthy();
+      expect(result.error).toBeUndefined();
+      expect(result.id).toBe(3);
+    });
+
+    it('should return error when accure exception', async () => {
+      podcastRepository.create.mockImplementation(() => {
+        throw Error();
+      });
+      const result = await podcastService.createPodcast(newPodcast);
+      expect(result.ok).toBeFalsy();
+      expect(result.error).toBe(serverErrorText);
+    });
+  });
+
+  describe('getPodcast', () => {
+    it('should return podcast', async () => {
+      podcastRepository.findOne.mockResolvedValue(allPodcast[0]);
+
+      const result = await podcastService.getPodcast(1);
+
+      expect(podcastRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(podcastRepository.findOne).toHaveBeenCalledWith(
+        { id: 1 },
+        expect.any(Object),
+      );
+      expect(result.ok).toBeTruthy();
+      expect(result.error).toBeUndefined();
+      expect(result.podcast).toEqual(allPodcast[0]);
+    });
+
+    it('shoud return error when id is not exist', async () => {
+      jest.spyOn(console, 'log');
+      podcastRepository.findOne.mockResolvedValue(null);
+
+      const result = await podcastService.getPodcast(1);
+
+      expect(result.ok).toBeFalsy();
+      expect(result.error).toBe('Podcast with id 1 not found');
+      expect(console.log).not.toHaveBeenCalled();
+    });
+
+    it('should return error when accure exception', async () => {
+      jest.spyOn(console, 'log');
+      podcastRepository.findOne.mockImplementation(() => {
+        throw Error();
+      });
+
+      const result = await podcastService.getPodcast(1);
+
+      expect(result.ok).toBeFalsy();
+      expect(result.error).toBe(serverErrorText);
+      expect(console.log).toHaveBeenCalledTimes(1);
     });
   });
 });
